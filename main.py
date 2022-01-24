bf2f947162908d9aee7f322fa7968d62e7aff84e
Chris Milios <cmilios@hotmail.com>
2022-01-24 20:51:26 +0200

    it makes mini transactions for now

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from FtxClient import FtxClient
 import sched, time
 
-key = 'YOUR API KEY HERE'
-secret = 'YOUR SECRET API KEY HERE'
+key = 'INSERT API KEY HERE'
+secret = 'INSERT SECRET KEY HERE'
 ftx = FtxClient(key, secret, 'Main account')
 
-s = sched.scheduler(time.time, time.sleep)
+pair = 'BTC/USD'
 
 bidav = 0
 askav = 0
 cup = 0
 cdn = 0
+upmoney = 0
+downmoney = 0
+sp = ''
+
+type_of_tran = ""
+
+tran_open = False
 
 while True:
-    order = ftx.get_orderbook('BTC/USD', 100)
+    order = ftx.get_orderbook(pair, 50)
     bidmoney = []
     askmoney = []
     for bid in order['bids']:
@@ -24,24 +31,67 @@ while True:
     bidav_new = sum(bidmoney) / len(bidmoney)
     askav_new = sum(askmoney) / len(askmoney)
 
+
     moneyscew_new = askav_new - bidav_new
     moneyscew = askav - bidav
-    moneyscew_limit1 = moneyscew * 0.1
-    moneyscew_limit2 = moneyscew * 1.5
+    moneyscew_limit1 = moneyscew * 0
+    moneyscew_limit2 = moneyscew * 2
     if moneyscew > 0:
         if moneyscew_new > moneyscew_limit2:
             print('signal up ' + str(cup) + " by " + str(moneyscew_new - moneyscew_limit2))
+            upmoney += moneyscew_new - moneyscew_limit2
             cup += 1
+            if not tran_open:
+                sp = ftx.get_single_market(pair)
+                print('TRANSACTION LONG MADE AT: ' + str(sp['price']))
+                tran_open = not tran_open
+                type_of_tran = 'LONG'
+
         elif moneyscew_new < moneyscew_limit1:
-            print('signal down ' + str(cdn) + ' with ' + str(moneyscew_new-moneyscew_limit1))
+            print('signal down ' + str(cdn) + ' with ' + str(moneyscew_new - moneyscew_limit1))
+            downmoney += moneyscew_new - moneyscew_limit1
             cdn += 1
+            if not tran_open:
+                sp = ftx.get_single_market(pair)
+                print('TRANSACTION SHORT MADE AT: ' + str(sp['price']))
+                tran_open = not tran_open
+                type_of_tran = 'SHORT'
+
     elif moneyscew < 0:
         if moneyscew_new < moneyscew_limit2:
-            print("signal down " + str(cdn) + " with " + str(moneyscew_new-moneyscew_limit2))
+            print("signal down " + str(cdn) + " with " + str(moneyscew_new - moneyscew_limit2))
+            downmoney += moneyscew_new - moneyscew_limit2
             cdn += 1
+            if not tran_open:
+                sp = ftx.get_single_market(pair)
+                print('TRANSACTION SHORT MADE AT: ' + str(sp['price']))
+                tran_open = not tran_open
+                type_of_tran = 'SHORT'
         elif moneyscew_new > moneyscew_limit1:
             print("signal up " + str(cup) + " by " + str(moneyscew_new - moneyscew_limit1))
+            upmoney += moneyscew_new - moneyscew_limit1
             cup += 1
+            if not tran_open:
+                sp = ftx.get_single_market(pair)
+                print('TRANSACTION LONG MADE AT: ' + str(sp['price']))
+                tran_open = not tran_open
+                type_of_tran = 'LONG'
+
+    print(upmoney + downmoney)
+    if type_of_tran == 'LONG':
+        if abs(upmoney) < abs(downmoney):
+            np = ftx.get_single_market(pair)
+            print('TRANSACTION LONG CLOSED AT: ' + str(np['price']))
+            print('RETURN: ' + str(np['price'] - sp['price']))
+            tran_open = not tran_open
+            break
+    elif type_of_tran == 'SHORT':
+        if abs(upmoney) > abs(downmoney):
+            np = ftx.get_single_market(pair)
+            print('TRANSACTION SHORT CLOSED AT: ' + str(np['price']))
+            print('RETURN: ' + str(sp['price'] - np['price']))
+            tran_open = not tran_open
+            break
 
     bidav = bidav_new
     askav = askav_new
